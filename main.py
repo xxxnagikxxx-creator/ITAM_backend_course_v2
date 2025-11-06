3aff9f74927e28028b0234a71a13fa33704c9e46
Trash Coder <xxxnagikxxx@gmail.com>
2025-11-06 22:33:58 +0300

    Change host to '0.0.0.0' for uvicorn server

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from presentations.fastapi_app import create_app
 
 
 def main() -> None:
-    uvicorn.run(create_app(), host="127.0.0.1", port=8000)
+    uvicorn.run(create_app(), host="0.0.0.0", port=8000)
 
 
 if __name__ == "__main__":
